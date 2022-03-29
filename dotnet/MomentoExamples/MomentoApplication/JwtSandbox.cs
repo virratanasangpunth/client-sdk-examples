cfe8d5c691aa3074b519224e6b893504d7964755
Vir Ratanasangpunth <vir@momentohq.com>
2022-03-29 14:14:01 -0700

    initial working code for generating jwts

---
+++ dotnet/MomentoExamples/MomentoApplication/JwtSandbox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MomentoApplication
+{
+    public class JwtSandbox
+    {
+
+        static void Main(string[] args)
+        {
+            RSAParameters privateKey;
+            using (var csp = new RSACryptoServiceProvider()
+            {
+                PersistKeyInCsp = false
+            })
+            {
+                privateKey = csp.ExportParameters(includePrivateParameters: true);
+            }
+            var securityKey = new RsaSecurityKey(privateKey);
+            var credentials = new SigningCredentials(securityKey, "RS256");
+            var JWTHeader = new JwtHeader(credentials);
+
+            var payload = new JwtPayload
+                {
+                    { "foo", "bar"},
+                };
+
+            var jwtToken = new JwtSecurityToken(JWTHeader, payload);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+            Console.WriteLine($"This is my JWT: {encodedJwt}");
+        }
+    }
+}
